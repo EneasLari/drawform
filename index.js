51bea1f49689aa468fac79fcacce6b5fdd5018d6
EneasLari <cs02367@cc.uoi.gr>
2021-01-04 03:02:40 +0200

    inital setup

---
+++ index.js
@@ -0,0 +1,25 @@
+var sidelnks=document.getElementsByClassName("linkheader");
+var currentvisible="";
+for (let index = 0; index < sidelnks.length; index++) {
+    const element = sidelnks[index];
+    if(element.id){
+        console.log("currentvisible")
+
+        
+        element.addEventListener("click",function() {
+            if(currentvisible){
+                currentvisible.style.display="none";
+            }
+            maketargetVsible(element.id)
+        })
+    }
+    
+}
+
+function maketargetVsible(buttonid)
+{
+    var elementtomakevisible=document.getElementsByName(buttonid)[0];
+    elementtomakevisible.style.display="block";
+    currentvisible=elementtomakevisible;
+    console.log(currentvisible)
+}
