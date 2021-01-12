146b30b0c9dec2e49d195d254706441cff428a17
EneasLari <cs02367@cc.uoi.gr>
2021-01-12 18:30:11 +0200

    more pages

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,31 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/nextproject', function(req, res, next) {
+    res.render('nextproject', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/storeselection', function(req, res, next) {
+    res.render('storeselection', { title: 'Express' });
+});
+
+
+/* GET home page. */
+router.get('/toolselection', function(req, res, next) {
+    res.render('toolselection', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/designpreview', function(req, res, next) {
+    res.render('designpreview', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/brandselection', function(req, res, next) {
+    res.render('brandselection', { title: 'Express' });
+});
+
+
 module.exports = router;
